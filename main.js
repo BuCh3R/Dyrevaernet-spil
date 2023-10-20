a63003e02c413328a34aef97850ba213bc3d9a2e
Egil W <egil.wendsjo@gmail.com>
2023-10-20 16:39:43 +0200

    Clean up main.js

--- main.js
+++ main.js
@@ -9,24 +9,16 @@ background.src = 'img/background.png';
 
 let xPos = 10;
 let yPos = 10;
-var hero = {xPos: 200, yPos: 200};
+let hero = {xPos: 200, yPos: 200};
 const canvas = document.getElementById("gameCanvas");
 const context = canvas.getContext("2d");
-
-
-char.onload = () => {
-    context.drawImage(background, 0, 0, 800, 800);
-    context.drawImage(char, hero.xPos, hero.yPos, 50, 50);
-    context.drawImage(tree, 50, 200, 64, 128);
-    char.onload = null;
-};
 
   // character // char move controls //
 function update(){
   // draw new character when moving //
     canvas.width=canvas.width;
     context.drawImage(background, 0, 0, 800, 800);
-    context.drawImage(char, hero.xPos, hero.yPos, 50, 50);
+    context.drawImage(char, hero.xPos, hero.yPos, 48, 48);
     context.drawImage(tree, 50, 200, 64, 128);
     requestAnimationFrame(update)
 }
