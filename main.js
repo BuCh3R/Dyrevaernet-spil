464af4c8fb17d809e2ef87acc1b38411262d880f
FlareRaxer <jonaskruse123@gmail.com>
2023-10-21 22:28:32 +0200

    Update main.js
    Added elements for house, rock and dog

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ char.src = 'img/player.png';
 const tree = new Image;
 tree.src = 'img/tree.png';
 
+const house = new Image; 
+house.src = 'img/house.png';
+
+const rock = new Image;
+rock.src = 'img/rock.png'; 
+
+const dog = new Image;
+dog.src = 'img/dog.png';
+
 const background = new Image;
 background.src = 'img/background.png';
 
@@ -20,6 +29,10 @@ function update(){
     context.drawImage(background, 0, 0, 800, 800);
     context.drawImage(char, hero.xPos, hero.yPos, 48, 48);
     context.drawImage(tree, 50, 200, 64, 128);
+    context.drawImage(house, 400, 400, 48, 48);
+    context.drawImage(rock, 350, 200, 32, 32);
+    context.drawImage(rock, 30, 150, 32, 32);
+    context.drawImage(dog, 310, 200, 24, 24); 
     requestAnimationFrame(update)
 }
 update()
